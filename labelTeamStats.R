37f992dbfd20be595025e587014e943c7e671efb
Seyi Adebayo <seyiaadebayo@gmail.com>
2017-03-23 21:51:53 -0400

    added additional logic to handle creation of testing data

--- labelTeamStats.R
+++ labelTeamStats.R
@@ -2,10 +2,14 @@ labelTeamStats <- function(prefix, df){
     col_names <- character()
     # setting up team stats
     for (j in colnames(df)){
-        if (j == 'TEAMID' & prefix == 'w'){
+        if (j == 'TEAMID' & prefix == 'w' ){
             col_names <- c(col_names, 'wteam')
+        }else if (j == 'TEAMID' & prefix == 'a'){
+            col_names <- c(col_names, 'TEAMID')
         }else if (j == 'TEAMID' & prefix == 'l'){
             col_names <- c(col_names, 'lteam')
+        }else if (j == 'TEAMID' & prefix == 'b'){
+            col_names <- c(col_names, 'TEAMID')
         }else if (j == 'season'){
             col_names <- c(col_names, j)
         }else{
