060f006eb1bf090cd1855393bbaee51b38f660d0
Seyi Adebayo <seyiaadebayo@gmail.com>
2017-03-23 21:51:05 -0400

    changed file name and added fxn to cr8 testing data

--- datacleanup.R
+++ datacleanup.R
@@ -13,61 +13,6 @@ str(train_df)
 require(lattice)
 with(train_df, xyplot(win,w_twpct))
 
-
-# prepTrainingData <- function(season.years) {
-#     train_df <- createTrainingMatchups(season.years)
-#     
-#     # Creating training data 
-#     require(stringr)
-#     
-#     # getting win pctage
-#     winPctage <- getWinPctage(season.years)
-#     
-#     # field goal percentages
-#     shootingpct <- getShootingPercentages(season.years)
-#     
-#     # assists and turnovers
-#     assistsNturnovers <- getAssistsNturnovers(season.years)
-#     
-#     # rebounds per game (offensive & defensive)
-#     reboundStats <- getReboundStats(season.years)
-#     
-#     # Num of wins in last 6
-#     wins_last_six_games_by_team <- winsLst6(season.years)
-#     
-#     # Seed in tourney
-#     seeding <- getTourneySeed(season.years)
-#     
-#     # Combining columns together
-#     stats <- Reduce(function(x, y) merge(x, y, by = c('TEAMID', 'season')), list(winPctage, shootingpct, assistsNturnovers,
-#                                                                                  reboundStats, wins_last_six_games_by_team, seeding))
-#     
-#     col_names.w <- labelTeamStats('w')
-#     col_names.l <- labelTeamStats('l')
-#     
-#     stats.w <- stats
-#     colnames(stats.w) <- col_names.w
-#     stats.l <- stats
-#     colnames(stats.l) <- col_names.l
-#     # creating final df
-#     team_metrics.w <- merge(train_df, stats.w, by = c('wteam', 'season'))
-#     team_metrics.t <- merge(team_metrics.w, stats.l, by = c('lteam', 'season'))
-#     
-#     # reordering columns
-#     wcols <- names(team_metrics.t[,grep('w_', names(team_metrics.t))])
-#     lcols <- names(team_metrics.t[,grep('l_', names(team_metrics.t))])
-#     team_metrics <- team_metrics.t[, c("season", "matchup", "win", "wteam", "lteam", wcols, lcols)]
-#     team_metrics <- team_metrics[order(team_metrics$season),]
-#     
-#     # chaning classes of certain vars
-#     # team_metrics$season <- as.factor(team_metrics$season)
-#     team_metrics$win <- as.factor(team_metrics$win)
-#     team_metrics$wteam <- as.factor(team_metrics$wteam)
-#     team_metrics$lteam <- as.factor(team_metrics$lteam)
-#     
-#     return(team_metrics)
-# }
-
 ######## building model #########  
 train <- subset(team_metrics, season <= 2014)
 train_df <- train[,c(-c(1:2,4:5))]
